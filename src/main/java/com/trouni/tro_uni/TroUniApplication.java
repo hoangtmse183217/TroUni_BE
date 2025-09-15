8c14e6bc9cc590a1defb7453f0b7e9e54804d2df
hoangtmse183217 <trinhminhhoang2004@gmail.com>
2025-09-15 23:19:19 +0700

    First commit

---
+++ src/main/java/com/trouni/tro_uni/TroUniApplication.java
@@ -0,0 +1,13 @@
+package com.trouni.tro_uni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TroUniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TroUniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trouni/tro_uni/TroUniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trouni.tro_uni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TroUniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
